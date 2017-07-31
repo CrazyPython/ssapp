509a48be82fef3c486edb635a90c1122d3f112ec
James <Jamtlu@gmail.com>
2017-07-31 11:56:35 -0400

    Update index.js

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require("./dev.env"),
-    port: 80,
+    port: 8090,
     autoOpenBrowser: false,
     assetsSubDirectory: "static",
     assetsPublicPath: "/",
